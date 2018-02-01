6f32883136e5e724957030f8f2354303f1a64b90
AleksCr <sashapolarear@gmail.com>
2018-02-01 09:50:27 +0300

    ship manage code

--- game_cycle.py
+++ game_cycle.py
@@ -27,10 +27,27 @@ while game_exit != 1 :
         info = input("id")
         print(info)
     elif action == "multiturn":
-        turns = input("input num of turns")
+        turns = input("input num of turns: ")
         for i in range(int(turns)):
             turn.gala_time.time_turn()
         print(str(i)+" turns passed")
+    elif action == "manage ship":
+        sh = input("input ship id: ")
+        for system in gal.systems:
+            for seach_ship in system.ships:
+                if seach_ship.id == int(sh):
+                    print("success! ship id is " + str(seach_ship.id) + ". Ship located in " + system.name + " system")
+                    new_sys = input("Enter new location system name: ")
+                    break
+        manage_ship = seach_ship
+        for system in gal.systems:
+            if system.name == new_sys:
+                manage_ship.new_location(system)
+                print("Hyper jump! New ship loc is " + system.name + " system!")
+    elif action == "test":
+        inp = input("num: ")
+        if int(inp) == 1:
+            print("kruto!")
     gal.galaxy2html("graph")
 
 # Race.create_civ(Race, gal)
