069ec158f9faf565f3ba495b906570249bca0eb9
AleksCr <sashapolarear@gmail.com>
2018-03-02 11:25:27 +0300

    fabrication init

---
+++ fabrication.py
@@ -0,0 +1,8 @@
+class Fabricate:
+    def __init__(self, str):
+        if str == "ship":
+            Fabricate_inner()
+
+class Fabricate_inner:
+    def __init__(self):
+        print("zaglushka")
