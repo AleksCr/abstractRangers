5de1592bfe3558dc68b35547fdbe411794285fa2
AleksCr <sashapolarear@gmail.com>
2018-03-06 00:04:05 +0300

    A little more abstract fabrication class

--- fabrication.py
+++ fabrication.py
@@ -4,7 +4,8 @@ import ship
 class Fabricate:
     def __init__(self, place, obj):
         print(obj.fab_points)
-        sh = ship.Ship(place)
-
-
+        # sh = ship.Ship(place)
+        sh = obj(place)
+        class_name = type(sh).__name__
+        print(class_name)
 
