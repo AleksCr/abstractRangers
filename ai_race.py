c8d27e2bbe01701a2ab3038bf72ed911e13edf87
AleksCr <sashapolarear@gmail.com>
2018-03-09 13:34:39 +0300

    "import" error

--- ai_race.py
+++ ai_race.py
@@ -1,4 +1,4 @@
-import game_cycle
+# import game_cycle
 import fabrication
 import ship
 
@@ -8,7 +8,7 @@ class AI_Race:
         ai_owner = race
 
     def ai_cycle(self):
-        for system in game_cycle.gal.systems:
+        for system in gal.systems:
             if system.planets:
                 for planet in system.planets:
                     if planet.owner != None:
